c126f3023b84f4667dc0b6059b3ff44fff348753
Jake Lehner <jakelehner@gmail.com>
2019-12-11 13:54:23 -0500

    Add docker-compose, custom aliases, and js framework cli's

--- after.sh
+++ after.sh
@@ -24,3 +24,18 @@
 # Install Node.js v10.x
 #curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 #sudo apt-get install -y nodejs
+
+# Install Docker-Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Docker Aliases
+if [ -d /home/vagrant/zsh-docker-aliases ]; then
+    echo "Docker Aliases already installed."
+else
+    git clone https://github.com/akarzim/zsh-docker-aliases.git /home/vagrant/zsh-docker-aliases
+fi
+
+# JS Framework Stuff
+sudo npm install -g @vue/cli
+sudo npm install -g @loopback/cli
